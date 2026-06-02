7384934dbf3dcc2ad93eef91babebfdea89bd4ef
许阳 <xuyang_wx@aliyun.com>
2026-06-02 15:19:07 +0800

    fix: 调整 DropdownMenu X轴偏移至12dp

--- app/src/main/java/com/mksword/passwordbook/MainActivity.kt
+++ app/src/main/java/com/mksword/passwordbook/MainActivity.kt
@@ -253,7 +253,7 @@ fun MainAppContent(userName: String, isLoggingOut: Boolean = false, onLogoutClic
                             DropdownMenu(
                                 expanded = expanded,
                                 onDismissRequest = { expanded = false },
-                                offset = DpOffset(x = 0.dp, y = 4.dp),
+                                offset = DpOffset(x = 12.dp, y = 4.dp),
                                 properties = androidx.compose.ui.window.PopupProperties(
                                     focusable = true,
                                     dismissOnBackPress = true,
