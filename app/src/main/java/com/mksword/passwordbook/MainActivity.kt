465c38d55f9ca98916ea8ae79a5e2e3589692dc5
许阳 <xuyang_wx@aliyun.com>
2026-06-02 13:49:03 +0800

    fix: 添加 background import

--- app/src/main/java/com/mksword/passwordbook/MainActivity.kt
+++ app/src/main/java/com/mksword/passwordbook/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.material.icons.Icons
